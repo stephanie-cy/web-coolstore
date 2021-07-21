bdd6b69bd75f314d4532ba51ad6f5ffd19ff3401
Stephanie <yangcao@redhat.com>
2021-07-21 16:47:22 -0400

    update gateway script
    Signed-off-by: Stephanie <yangcao@redhat.com>

--- gateway-vertx/deploy.sh
+++ gateway-vertx/deploy.sh
@@ -4,21 +4,38 @@
 
 DIRECTORY=`dirname $0`
 PROJECT_NAME=$1
+OPERATORS=$(oc get subscription -n openshift-operators)
 
 cd ${DIRECTORY}
 
+
 odo project set ${PROJECT_NAME}
-odo delete --all --force
+
+if [[ $OPERATORS == *"rh-service-binding-operator"* ]]; then
+    odo unlink catalog &> /dev/null
+    odo unlink inventory &> /dev/null
+else
+    odo config unset --env CATALOG_COOLSTORE_SERVICE_HOST --env CATALOG_COOLSTORE_SERVICE_PORT &> /dev/null
+    odo config unset --env INVENTORY_COOLSTORE_SERVICE_HOST --env INVENTORY_COOLSTORE_SERVICE_PORT &> /dev/null
+fi
+odo delete --all --force &> /dev/null
 odo create gateway --app coolstore
 
-CATALOG_HOST=$(oc get svc catalog-coolstore -o jsonpath={.spec.clusterIP})
-CATALOG_PORT=$(oc get svc catalog-coolstore -o jsonpath={.spec.ports[].port})
-INVENTORY_HOST=$(oc get svc inventory-coolstore -o jsonpath={.spec.clusterIP})
-INVENTORY_PORT=$(oc get svc inventory-coolstore -o jsonpath={.spec.ports[].port})
+if [[ $OPERATORS == *"rh-service-binding-operator"* ]]; then
+    odo push
+    odo link inventory
+    odo link catalog
+    odo push
+else
+    CATALOG_HOST=$(oc get svc catalog-coolstore -o jsonpath={.spec.clusterIP})
+    CATALOG_PORT=$(oc get svc catalog-coolstore -o jsonpath={.spec.ports[].port})
+    INVENTORY_HOST=$(oc get svc inventory-coolstore -o jsonpath={.spec.clusterIP})
+    INVENTORY_PORT=$(oc get svc inventory-coolstore -o jsonpath={.spec.ports[].port})
 
-odo config set --env COMPONENT_CATALOG_HOST=$CATALOG_HOST --env COMPONENT_CATALOG_PORT=$CATALOG_PORT
-odo config set --env COMPONENT_INVENTORY_HOST=$INVENTORY_HOST--env COMPONENT_INVENTORY_PORT=$INVENTORY_PORT
-
-odo push
+    odo config set --env CATALOG_COOLSTORE_SERVICE_HOST=$CATALOG_HOST --env CATALOG_COOLSTORE_SERVICE_PORT=$CATALOG_PORT
+    odo config set --env INVENTORY_COOLSTORE_SERVICE_HOST=$INVENTORY_HOST--env INVENTORY_COOLSTORE_SERVICE_PORT=$INVENTORY_PORT
+    
+    odo push
+fi
 
 echo "Gateway Vertx Deployed"
