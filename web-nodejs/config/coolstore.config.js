c05fbf0c99f9a46c9d406bd7ccd9ca87cc5c6bf7
Stephanie <yangcao@redhat.com>
2021-07-28 14:25:00 -0400

    use parent instead
    Signed-off-by: Stephanie <yangcao@redhat.com>

--- web-nodejs/config/coolstore.config.js
+++ web-nodejs/config/coolstore.config.js
@@ -2,7 +2,6 @@ const { ContextExclusionPlugin } = require("webpack");
 
 var config = {
     API_ENDPOINT: 'gateway-coolstore-' + process.env.OPENSHIFT_BUILD_NAMESPACE,
-    // API_ENDPOINT: process.env.GATEWAY_COOLSTORE_SERVICE_HOST + ":" + process.env.GATEWAY_COOLSTORE_SERVICE_PORT,
     SECURE_API_ENDPOINT: 'secure-gateway-coolstore-' + process.env.SECURE_COOLSTORE_GW_SERVICE,
     SSO_ENABLED: process.env.SSO_URL ? true : false
 };
@@ -19,6 +18,4 @@ if (process.env.SECURE_COOLSTORE_GW_ENDPOINT != null) {
 } else if (process.env.SECURE_COOLSTORE_GW_SERVICE != null) {
     config.SECURE_API_ENDPOINT = process.env.SECURE_COOLSTORE_GW_SERVICE + '-' + process.env.OPENSHIFT_BUILD_NAMESPACE;
 }
-// config.API_ENDPOINT = "172.30.54.91:8080"
-// config.SECURE_API_ENDPOINT =  "172.30.54.91:8080"
 module.exports = config;
