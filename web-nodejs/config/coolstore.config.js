af4ba0734d99598ffed335f7d68737d1169208e2
Stephanie <yangcao@redhat.com>
2021-08-04 16:23:29 -0400

    update the gateway predicted url endpoint
    Signed-off-by: Stephanie <yangcao@redhat.com>

--- web-nodejs/config/coolstore.config.js
+++ web-nodejs/config/coolstore.config.js
@@ -7,7 +7,7 @@ var config = {
 };
 
 if (process.env.DEVWORKSPACE_ID != null) {
-    config.API_ENDPOINT = process.env.DEVWORKSPACE_ID + '-3'
+    config.API_ENDPOINT = process.env.DEVWORKSPACE_ID + '-2'
 } else if (process.env.COOLSTORE_GW_ENDPOINT != null) {
     config.API_ENDPOINT = process.env.COOLSTORE_GW_ENDPOINT;
 } else if (process.env.COOLSTORE_GW_SERVICE != null) {
