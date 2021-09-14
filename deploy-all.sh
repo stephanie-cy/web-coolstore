1ac47d8edeafb457507736f728c7a4da39283226
Stephanie <yangcao@redhat.com>
2021-09-14 16:39:06 -0400

    add deploy-all script
    Signed-off-by: Stephanie <yangcao@redhat.com>

---
+++ deploy-all.sh
@@ -0,0 +1,22 @@
+##########################
+#  Deploy all components #
+##########################
+
+PROJECT_NAME=$1
+
+echo "creating namespace: ${PROJECT_NAME}"
+oc new-project $PROJECT_NAME
+
+PROJECT_DIR=`dirname $0`
+
+echo "deploying catalog component"
+cd $PROJECT_DIR/catalog-spring-boot && ./deploy.sh $PROJECT_NAME
+
+echo "deploying inventory component"
+cd $PROJECT_DIR/inventory-quarkus && ./deploy.sh $PROJECT_NAME
+
+echo "deploying gateway component"
+cd $PROJECT_DIR/gateway-vertx && ./deploy.sh $PROJECT_NAME
+
+echo "deploying web-ui component"
+cd $PROJECT_DIR/web-nodejs && ./deploy.sh $PROJECT_NAME
